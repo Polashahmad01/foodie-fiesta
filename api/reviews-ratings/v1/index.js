fcfc3257fc56afc8bf5d6d51c16674da3ce7773b
Polashahmad01 <dev.polashahmad@gmail.com>
2023-10-19 14:51:30 +0600

    Update updateReviewsAndRatings and deleteReviewsAndRatings functions

--- api/reviews-ratings/v1/index.js
+++ api/reviews-ratings/v1/index.js
@@ -35,11 +35,37 @@ const createReviewsAndRatings = async (req, res) => {
 }
 
 const updateReviewsAndRatings = async (req, res) => {
-  res.status(200).json({ success: true, message: "Update a review for a product" })
+  const { client, db } = await getMongoConnection()
+  try {
+    const { productId, reviewId } = req.params
+    const review = await db.collection("reviews").findOneAndUpdate(
+      { _id: new ObjectId(reviewId), productId: new ObjectId(productId) },
+      { $set: req.body },
+      { returnOriginal: false }
+    )
+    res.status(200).json({ success: true, data: review })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ success: false, message: error.message })
+  } finally {
+    await client.close()
+  }
 }
 
 const deleteReviewsAndRatings = async (req, res) => {
-  res.status(200).json({ success: true, message: "Delete a review for a product" })
+  const { client, db } = await getMongoConnection()
+  try {
+    const { productId, reviewId } = req.params
+    const review = await db.collection("reviews").findOneAndDelete(
+      { _id: new ObjectId(reviewId), productId: new ObjectId(productId) }
+    )
+    res.status(200).json({ success: true, data: review })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ success: false, message: error.message })
+  } finally {
+    await client.close()
+  }
 }
 
 router.get("/products/:productId/reviews", getAllReviewsAndRatings)
